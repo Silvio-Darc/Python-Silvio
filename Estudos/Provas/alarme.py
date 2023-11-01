45623ecea0de1c544008139731d6aeb33e7b7fe3
Silvio-Darc <149602067+Silvio-Darc@users.noreply.github.com>
2023-11-01 12:30:36 -0300

    Update alarme.py

--- Estudos/Provas/alarme.py
+++ Estudos/Provas/alarme.py
@@ -1,3 +1,7 @@
+# entrada vai ser algo como 23 32 0 12 
+# o primeiro e o segundo valor são as horas e minutos do horário atual
+# o terceiro e o quarto valor são as horas e minutos do alarme selecionado para o futuro
+# o código deve retornar quanto tempo falta até o alarme tocar, em minutos
 h1, m1, h2, m2 = list(map(int, input().split()))
 while h1 + m1 + h2 + m2 != 0:
     if h2 < h1:
