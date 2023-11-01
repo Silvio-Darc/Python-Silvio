321ec60ddf6e8da861b43e1fa49d8da78b242ca6
Lucas Licastro <lucas.licastro@tempo.io>
2023-11-01 13:06:28 -0300

    alarme melhorado

--- Estudos/Provas/alarme.py
+++ Estudos/Provas/alarme.py
@@ -1,18 +1,20 @@
+def para_minutos(horas, minutos):
+    return horas * 60 + minutos
+
+
 def alarme(h1, m1, h2, m2):
-    if h2 < h1:
-        h2 += 24
-    if h2 == h1 and m2 < m1:
+    if h2 < h1 or (h2 == h1 and m2 < m1):
         h2 += 24
 
-    minutos_1 = h1 * 60 + m1
-    minutos_2 = h2 * 60 + m2
-
-    minutos_restantes = minutos_2 - minutos_1
+    minutos_atual = para_minutos(h1, m1)
+    minutos_alarme = para_minutos(h2, m2)
+    minutos_restantes = minutos_alarme - minutos_atual
     print(minutos_restantes)
     return minutos_restantes
 
 
 assert alarme(0, 0, 0, 1) == 1
+assert alarme(0, 1, 0, 0) == 1439
 assert alarme(0, 0, 1, 1) == 61
 assert alarme(0, 0, 23, 59) == 1439
 assert alarme(23, 59, 0, 0) == 1
