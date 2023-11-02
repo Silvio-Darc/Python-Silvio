dcbcab643a1c6d1fdedd1707d896105bcb83771d
Lucas Licastro <lucas.licastro@tempo.io>
2023-11-01 23:12:11 -0300

    doces melhorada + enunciado

--- Estudos/Provas/doces.py
+++ Estudos/Provas/doces.py
@@ -4,31 +4,29 @@
 # repita o processo ate todos tenham o msm numero de doces
 # quantas rodadas foram necessarias?
 # se for impossivel imprime -1
+# para sair digite 0
 
-n = int(input())
-while n != 0:
-    entrada = list(map(int, input().split()))
-    if sum(entrada) % n == 0:
-        rodadas = 0
-        lista_ideal = []
-        n_ideal = sum(entrada) / n
-        for i in range(n):
-            n_ideal = int(sum(entrada) / n)
-            lista_ideal.append(n_ideal)
-        while entrada != lista_ideal:
-            for i in range(n):
-                if i + 1 > n - 1:
-                    i -= n - 1
-                if entrada[i + 1] - entrada[i] > 0:
-                    rodadas += 1
-                    entrada[i + 1] -= 1
-                    entrada[i] += 1
-                elif entrada[i + 1] - entrada[i] < 0:
-                    rodadas += 1
-                    entrada[i + 1] += 1
-                    entrada[i] -= 1
-        print(rodadas)
-        n = int(input())
-    else:
+def doces(lista):
+    rodadas = 1
+    if sum(lista) % len(lista) != 0:
         print(-1)
-        n = int(input())
+        return -1
+    else:
+        pobre = min(lista)
+        rico = max(lista)
+        while pobre != rico:
+            lista[lista.index(pobre)] += 1
+            lista[lista.index(rico)] -= 1
+            print(lista)
+            rodadas += 1
+            pobre = min(lista)
+            rico = max(lista)
+    print(rodadas)
+    return rodadas
+
+
+assert doces([2, 2, 2]) == 1
+assert doces([1, 2, 3]) == 2
+assert doces([2, 3, 4, 5]) == -1
+assert doces([2, 3, 4, 6]) == -1
+assert doces([2, 3, 5, 6]) == 4
