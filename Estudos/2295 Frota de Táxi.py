ba5a26c61224bd430e74225b2e7933421991e899
Silvio-Darc <silviodarcangelo@id.uff.br>
2023-11-07 22:05:24 -0300

    exercício do beecrowd

---
+++ Estudos/2295 Frota de Táxi.py
@@ -0,0 +1,13 @@
+a, g, ra, rg = map(float, input().split())
+
+if (a / ra < g / rg):
+    print('A')
+else:
+    print('G')
+
+
+#Quanto você anda com 1 litro = RA ou RG
+#Se eu ando RA KMS gastando a reais
+#           1 km = x
+#           a = RAx
+#           a / RA = x
