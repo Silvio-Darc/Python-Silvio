82c1ec70d0e3ac7063bb87ea23cced6d0f7ed0fb
Silvio-Darc <silviodarcangelo@id.uff.br>
2023-11-08 20:50:48 -0300

    frequencia de letras

---
+++ Estudos/1255 Frequência de Letras.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    palavra = input().lower()
+    letras = []
+    frequencia_letras = []
+    palavra_final = []
+    for i in palavra:
+        letras.append(ord(i))
+    for i in range(26):
+        frequencia_letras.append(letras.count(97 + i))
+    valor_maximo = max(frequencia_letras)
+    for i in range(26):
+        if frequencia_letras[i] == valor_maximo:
+            palavra_final.append(chr(97+i))
+    print(''.join(palavra_final))
