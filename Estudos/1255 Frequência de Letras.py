8937874c569558e21c2679367d7f110cfdab1ac5
Silvio-Darc <silviodarcangelo@id.uff.br>
2023-11-08 20:52:20 -0300

    frequencia de letras com comentario
    (cherry picked from commit aec7e7a93c6ed84075ae715987939dcf5d3d286b)
    
    # Conflicts:
    #	Estudos/1255 Frequência de Letras.py

--- Estudos/1255 Frequência de Letras.py
+++ Estudos/1255 Frequência de Letras.py
@@ -1,27 +1,15 @@
-def freq(word_input):
-    letters_dict = {}
-    max_frequency = 0
-    # Build a dict of all 26 letters of the alphabet (you can use
-    for letter_ord in range(ord('a'), ord('z')):
-        letters_dict[chr(letter_ord)] = 0
-    # Normalize to lowercase
-    word = word_input.lower()
-    for letter in word:
-        # If that letter is in the dict
-        if letter in letters_dict:
-            # Increase the frequency for that letter
-            letters_dict[letter] += 1
-            # and decide if it is the new max_frequency
-            max_frequency = max(max_frequency, letters_dict[letter])
-    final = ''
-    for letter in letters_dict:
-        # if the frequency of that letter is the same as the max_frequency append to the final string
-        if letters_dict[letter] == max_frequency:
-            final += letter
-    print(final)
-    return final
-
-
-assert freq("Computers account for only 5% of the country's commercial electricity consumption") == "co"
-assert freq("Input") == "inptu"
-assert freq("frequency letters") == "e"
+# Computers account for only 5% of the country's commercial electricity consumption.
+for _ in range(int(input())):
+    palavra = input().lower()
+    letras = []
+    frequencia_letras = []
+    palavra_final = []
+    for i in palavra:
+        letras.append(ord(i))
+    for i in range(26):
+        frequencia_letras.append(letras.count(97 + i))
+    valor_maximo = max(frequencia_letras)
+    for i in range(26):
+        if frequencia_letras[i] == valor_maximo:
+            palavra_final.append(chr(97+i))
+    print(''.join(palavra_final))
