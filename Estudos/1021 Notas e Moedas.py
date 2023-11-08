641c4017d83d7763e7b082052741b0e3829a4502
Silvio-Darc <silviodarcangelo@id.uff.br>
2023-11-08 18:55:51 -0300

    correção do exercício notas e moedas. corrigido erro de imprecisão de float

--- Estudos/1021 Notas e Moedas.py
+++ Estudos/1021 Notas e Moedas.py
@@ -2,14 +2,15 @@ valor = float(input())
 notas = [0, 0, 0, 0, 0, 0]
 moedas = [0, 0, 0, 0, 0, 0]
 tipos_notas = [100, 50, 20, 10, 5, 2]
-tipos_moedas = [1, 0.5, 0.25, 0.10, 0.05, 0.01]
+tipos_moedas = [100, 50,25, 10, 5, 1]
 for i in range(6):
     notas[i] = valor // tipos_notas[i]
     valor %= tipos_notas[i]
+valor *= 100
 for i in range(6):
-    moedas[i] = valor / tipos_moedas[i]
+    moedas[i] = valor // tipos_moedas[i]
     valor %= tipos_moedas[i]
-    valor = round(valor, 2)
+
 
 print("NOTAS:")
 for i in range(6):
@@ -17,4 +18,4 @@ for i in range(6):
 
 print("MOEDAS:")
 for i in range(6):
-    print(f"{moedas[i]:.0f} moeda(s) de R$ {tipos_moedas[i]:.2f}")
+    print(f"{moedas[i]:.0f} moeda(s) de R$ {tipos_moedas[i] / 100:.2f}")
