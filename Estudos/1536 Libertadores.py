379011fef3206b0af800f538d285195ad85fb671
silvd <silviodarcangelo@id.uff.br>
2024-04-05 22:10:35 -0300

    Libertadores melhorado

--- Estudos/1536 Libertadores.py
+++ Estudos/1536 Libertadores.py
@@ -1,45 +1,35 @@
-# Critério de desempate:
-    # Normal) pontos.
-    # 1) saldo de gols (número de gols a favor menos o número de gols contra).
-    # 2) mais gols marcados na casa do adversário.
-    # 3) disputa por pênaltis.
-
-
-def comparar(time1, time2):
-    if time1 > time2:
+def verificar(time1: int, time2: int) -> bool:
+    if time1 == time2:
+        return False
+    elif time1 > time2:
         print("Time 1")
-    elif time1 < time2:
-        print("Time 2")
     else:
-        return 1
+        print("Time 2")
+    return True
 
+
+def calcularPontos (placares: list, time: int, adversario: int):
+    pontos = 0
+    for placar in placares:
+        if placar[time] > placar[adversario]:
+            pontos += 3
+        elif placar[time] == placar[adversario]:
+            pontos += 1
+    return pontos
 
 
 for _ in range(int(input())):
-    jogo1 = list(map(int, input().split(" x ")))
-    jogo2 = list(map(int, input().split(" x ")))
-    pontosTime1 = 0
-    pontosTime2 = 0
-    golsTime1 = jogo1[0] + jogo2[1]
-    golsTime2 = jogo1[1] + jogo2[0]
-    if jogo1[0] > jogo1[1]:
-        pontosTime1 += 3
-    elif jogo1[0] == jogo1[1]:
-        pontosTime1 += 1
-        pontosTime2 += 1
-    else:
-        pontosTime2 += 3
-    if jogo2[0] > jogo2[1]:
-        pontosTime2 += 3
-    elif jogo2[0] == jogo2[1]:
-        pontosTime1 += 1
-        pontosTime2 += 1
-    else:
-        pontosTime1 += 3
+    placarIda = list(map(int, input().split(" x ")))
+    placarVolta = list(map(int, input().split(" x ")))
+    placarVolta.reverse()
+    pontosTime1 = calcularPontos([placarIda, placarVolta], 0, 1)
+    pontosTime2 = calcularPontos([placarIda, placarVolta], 1, 0)
+    saldoTime1 = placarIda[0] + placarVolta[0]
+    saldoTime2 = placarIda[1] + placarVolta[1]
+    golsFeitosForaDeCasaTime1 = placarVolta[0]
+    golsFeitosForaDeCasaTime2 = placarIda[1]
 
-    if comparar(pontosTime1, pontosTime2) == 1:
-        if comparar(golsTime1, golsTime2) == 1:
-            if comparar(jogo2[1], jogo1[1]) == 1:
+    if not verificar(pontosTime1, pontosTime2):
+        if not verificar(saldoTime1, saldoTime2):
+            if not verificar(golsFeitosForaDeCasaTime1, golsFeitosForaDeCasaTime2):
                 print("Penaltis")
-
-
