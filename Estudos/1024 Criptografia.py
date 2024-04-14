7264a0a370a0e127fb7eba1df9d8cba14b93c732
silvd <silviodarcangelo@id.uff.br>
2024-04-14 16:51:30 -0300

    Criptografia com 2 for

--- Estudos/1024 Criptografia.py
+++ Estudos/1024 Criptografia.py
@@ -2,18 +2,15 @@ testes = int(input())
 for teste in range(testes):
     entrada = input()
     resultado1 = ""
-    resultado2 = ""
     resultadoFinal = ""
-    for letra in entrada:
-        if ord(letra) in range(65, 91) or ord(letra) in range(97, 123):
-            resultado1 += chr(ord(letra) + 3)
+    for posletra in range(len(entrada)):
+        if ord(entrada[posletra]) < 65 or ord(entrada[posletra]) > 122 or 91 <= ord(entrada[posletra]) <= 96:
+            resultado1 = entrada[posletra] + resultado1
         else:
-            resultado1 += letra
-    for letra in resultado1:
-        resultado2 = letra + resultado2
-    for posLetra in range(len(resultado2)):
-        if posLetra < (len(resultado2) // 2):
-            resultadoFinal += resultado2[posLetra]
+            resultado1 = chr(ord(entrada[posletra]) + 3) + resultado1
+    for posLetra in range(len(resultado1)):
+        if posLetra < (len(resultado1) // 2):
+            resultadoFinal += resultado1[posLetra]
         else:
-            resultadoFinal += chr(ord(resultado2[posLetra]) - 1)
+            resultadoFinal += chr(ord(resultado1[posLetra]) - 1)
     print(resultadoFinal)
